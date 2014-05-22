35242bc6d725ed8b67a322f74620d29ee86798c7
alex <argriffi@ncsu.edu>
2014-05-22 14:43:19 -0400

    move more code back into nxblink.em

--- examples/p53/p53em.py
+++ examples/p53/p53em.py
@@ -41,6 +41,8 @@ from algopy import log, exp, square
 
 import nxblink
 from nxblink.summary import Summary
+from nxblink.em import (
+        get_ll_dwell, get_ll_trans, get_ll_root, get_expected_rate)
 
 # algopy boilerplate
 def eval_grad(f, theta):
@@ -79,89 +81,14 @@ def objective(summary, edges, genetic_code, log_params):
     pre_Q = get_pre_Q(genetic_code, kappa, omega, A, C, G, T)
     distn = get_distn(genetic_code, kappa, omega, A, C, G, T)
 
-    # construct the blink distribution with the right data type
-    blink_distn = algopy.zeros(2, dtype=distn)
-    blink_distn[0] = blink_off / (blink_on + blink_off)
-    blink_distn[1] = blink_on / (blink_on + blink_off)
-
-    # compute the expected rate of the unnormalized pre-rate-matrix
-    expected_rate = get_expected_rate(pre_Q, distn)
-
-    # initialize expected log likelihood using the right data type
-    ell = algopy.zeros(3, dtype=distn)[0]
-
-    # root primary state contribution to expected log likelihood
-    obs = algopy.zeros_like(distn)
-    for state, count in summary.root_pri_to_count.items():
-        if count:
-            ell = ell + count * log(distn[state])
-
-    # root blink state contribution to expected log likelihood
-    if summary.root_off_count:
-        ell = ell + summary.root_off_count * log(blink_distn[0])
-    if summary.root_xon_count:
-        ell = ell + summary.root_xon_count * log(blink_distn[1])
-
-    # contributions per edge
-    for edge, edge_rate in zip(edges, edge_rates):
-
-        # extract edge summaries for transitions
-        pri_trans = summary.edge_to_pri_trans[edge]
-        off_xon_trans = summary.edge_to_off_xon_trans[edge]
-        xon_off_trans = summary.edge_to_xon_off_trans[edge]
-
-        # extract edge summaries for dwell times
-        pri_dwell = summary.edge_to_pri_dwell[edge]
-        off_xon_dwell = summary.edge_to_off_xon_dwell[edge]
-        xon_off_dwell = summary.edge_to_xon_off_dwell[edge]
-
-        # Initialize the total number of all types of transitions.
-        # This is for making adjustments according to the edge rate
-        # and the expected rate of the pre-rate-matrix.
-        transition_count_sum = 0
-
-        # contribution of primary transition summary to expected log likelihood
-        for sa in pri_trans:
-            for sb in pri_trans[sa]:
-                count = pri_trans[sa][sb]['weight']
-                transition_count_sum += count
-                if count:
-                    ell = ell + count * log(pre_Q[sa, sb])
-
-        # contribution of blink transition summary to expected log likelihood
-        if off_xon_trans:
-            transition_count_sum += off_xon_trans
-            ell = ell + off_xon_trans * log(blink_on)
-        if xon_off_trans:
-            transition_count_sum += xon_off_trans
-            ell = ell + xon_off_trans * log(blink_off)
-
-        # add the adjustment for edge-specific rate and pre-rate-matrix scaling
-        if transition_count_sum:
-            ell = ell + transition_count_sum * log(edge_rate / expected_rate)
-        
-        # compute a scaled sum of dwell times
-        ell_dwell = algopy.zeros(1, dtype=distn)[0]
-
-        # contribution of dwell times associated with primary transitions
-        for sa in pri_dwell:
-            for sb in pri_dwell[sa]:
-                elapsed = pri_dwell[sa][sb]['weight']
-                if elapsed:
-                    ell_dwell = ell_dwell + elapsed * pre_Q[sa, sb]
-
-        # contribution of dwell times associated with tolerance transitions
-        if off_xon_dwell:
-            ell_dwell = ell_dwell + off_xon_dwell * blink_on
-        if xon_off_dwell:
-            ell_dwell = ell_dwell + xon_off_dwell * blink_off
-
-        # Add the dwell time contribution,
-        # adjusted for edge-specific rate and pre-rate-matrix scaling.
-        ell = ell - ell_dwell * (edge_rate / expected_rate)
-
-    # return the negative expected log likelihood
-    return -ell
+    # compute expected negative log likelihood
+    ll_root = get_ll_root(summary, distn, blink_on, blink_off)
+    ll_dwell = get_ll_dwell(summary, pre_Q, distn, blink_on, blink_off,
+            edges, edge_rates)
+    ll_trans = get_ll_trans(summary, pre_Q, distn, blink_on, blink_off,
+            edges, edge_rates)
+    neg_ll = -(ll_root + ll_dwell + ll_trans)
+    return neg_ll
 
 
 def maximization_step(summary, genetic_code,
@@ -328,8 +255,3 @@ def get_distn(genetic_code, kappa, omega, A, C, G, T):
     # return the normalized distribution
     distn = weights / weights.sum()
     return distn
-
-
-def get_expected_rate(pre_Q, distn):
-    return algopy.dot(pre_Q.sum(axis=1), distn)
-
