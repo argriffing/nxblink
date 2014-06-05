9b04968df3da7d1b6796a7aebd2304be4cd923fc
alex <argriffi@ncsu.edu>
2014-06-05 13:26:46 -0400

    finish but not test forward sampling from the blinking model

--- nxblink/fwdsample.py
+++ nxblink/fwdsample.py
@@ -6,7 +6,57 @@ The samples are conditional on the initial state at the root.
 """
 from __future__ import division, print_function, absolute_import
 
-from .trajectory import LightTrajectory
+from .trajectory import LightTrajectory, Event
+
+
+def gen_potential_transitions(
+        track_to_state, primary_name, primary_to_tol, Q_primary, Q_blink):
+    """
+    Generate potential transitions given the current state and the process.
+
+    Collect all allowed transitions and their rates.
+    The entries in the collection will be structured like
+    ((track name, initial state, final state), rate).
+    This collection will be used as follows.
+    First, it will be used to compute the total rate,
+    which in turn will be used to sample a transition time.
+    If the transition time does not exceed the tail endpoint
+    of the edge, then a transition will be added to the event
+    list of the current edge for its corresponding track.
+    The transition will be picked according to a weighted
+    choice whose weights are proportional to the transition rate.
+
+    """
+    # Extract properties of the current state.
+    current_pri_state = track_to_state[primary_name]
+    current_tol_class = primary_to_tol[current_pri_state]
+
+    # Yield tolerated primary process transitions.
+    for sa in Q_primary:
+        for sb in Q_primary[sa]:
+            sb_tol_class = primary_to_tol[sb]
+            if track_to_state[sb_tol_class]:
+                rate = Q_primary[sa][sb]['weight']
+                trans = (primary_name, sa, sb)
+                yield trans, rate
+
+    # Yield tolerance process transitions.
+    tol_classes = set(primary_to_tol.values())
+    for tol_class in tol_classes:
+
+        # Gain of tolerance is always allowed.
+        if not track_to_state[tol_class]:
+            trans = (tol_class, 0, 1)
+            rate = Q_blink[0][1]['weight']
+            yield trans, rate
+
+        # Loss of tolerance is allowed, except for the
+        # tolerance class corresponding to the current primary state.
+        if track_to_state[tol_class]:
+            if track_to_state[current_tol_class]:
+                trans = (tol_class, 1, 0)
+                rate = Q_blink[1][0]['weight']
+                yield trans, rate
 
 
 def gen_forward_samples(model, seq_of_track_to_root_state):
@@ -107,6 +157,7 @@ def gen_forward_samples(model, seq_of_track_to_root_state):
 
         # Aggregate the tracks.
         tracks = [pri_track] + tol_tracks
+        name_to_track = dict((t.name, t) for t in tracks)
 
         # Iterate over edges, from the root towards the leaves.
         for edge in nx.bfs_edges(T, root):
@@ -127,26 +178,48 @@ def gen_forward_samples(model, seq_of_track_to_root_state):
             while True:
 
                 # Collect all allowed transitions and their rates.
-                # The entries in the collection will be structured like
-                # ((track name, initial state, final state), rate).
-                # This collection will be used as follows.
-                # First, it will be used to compute the total rate,
-                # which in turn will be used to sample a transition time.
-                # If the transition time does not exceed the tail endpoint
-                # of the edge, then a transition will be added to the event
-                # list of the current edge for its corresponding track.
-                # The transition will be picked according to a weighted
-                # choice whose weights are proportional to the transition rate.
-                pot_trans = []
+                pot = list(gen_potential_transitions(
+                    track_to_state, pri_track.name,
+                    primary_to_tol, Q_primary, Q_blink))
+                
+                # Get the total rate out of the current state.
+                # This includes primary and tolerance process rates.
+                rate = sum(r for t, r in pot)
 
-                # Add transitions corresponding to 
+                # Sample a wait time.
+                # The wait time will be small if the rate is large.
+                scale = 1/rate
+                wait_time = np.random.exponential(scale)
+                tm_ev = tm + wait_time
 
-                # Compute the total rate out of the current state.
-                # This includes rates to synonymous codons,
-                # rates to tolerated nonsynonymous codons,
-                # rates of tolerance loss, and rates of tolerance gain.
-                total_primary_rate = 0
-                for sa in Q_primary:
-                    for sb in Q_primary[sa]:
+                # If the wait time puts the time of the transition
+                # past the tail endpoint of the edge,
+                # then we have finished sampling events on this edge.
+                if tm_ev > tm_tail:
+                    break
 
+                # Sample the transition proportionally to rate.
+                n = len(pot)
+                ts, rates = zip(*pot)
+                weights = np.array(rates)
+                distn = weights / weights.sum()
+                idx = np.random.choice(range(n), p=distn)
+                track_name, sa, sb = ts[idx]
+                track = name_to_track[track_name]
+
+                # Create the new event object,
+                # add the event to the appropriate track,
+                # update the current time,
+                # and update the current state.
+                ev = Event(track, tm_ev, sa, sb)
+                track.events[edge].append(ev)
+                tm = tm_ev
+                track_to_state[track_name] = sb
+
+            # Update the history at the tail endpoint of the edge.
+            for track in tracks:
+                track.history[nb] = track_to_state[track.name]
+
+    # Return the sampled trajectories.
+    return pri_track, tol_tracks
 
