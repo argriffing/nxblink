dcee0acc0c5fe5509cf3e3e126a039f707e761de
alex <argriffi@ncsu.edu>
2014-07-28 14:09:42 -0400

    use nxctmctree for trajectory and event base classes

--- nxblink/trajectory.py
+++ nxblink/trajectory.py
@@ -10,48 +10,8 @@ import networkx as nx
 from .util import (
         get_total_rates, get_omega, get_uniformized_P_nx, get_identity_P_nx)
 
-
-class LightTrajectory(object):
-    """
-    This base class is used by itself for testing.
-
-    Parameters
-    ----------
-    name : hashable
-        name of the trajectory
-    history : dict
-        map from structural node to current state.
-    events : dict
-        map from structural edge to list of events on that edge
-
-    """
-    def __init__(self, name=None, history=None, events=None):
-        self.name = name
-        self.history = history
-        self.events = events
-
-    def remove_self_transitions(self):
-        edges = set(self.events)
-        for edge in edges:
-            events = self.events[edge]
-            self.events[edge] = [ev for ev in events if ev.sb != ev.sa]
-
-    def clear_state_labels(self):
-        """
-        Clear the state labels but not the event times.
-
-        """
-        nodes = set(self.history)
-        edges = set(self.events)
-        for v in nodes:
-            self.history[v] = None
-        for edge in edges:
-            for ev in self.events[edge]:
-                ev.sa = None
-                ev.sb = None
-
-    def __str__(self):
-        return 'Track(%s)' % self.name
+import nxctmctree
+from nxctmctree.trajectory import Event, LightTrajectory
 
 
 class Trajectory(LightTrajectory):
@@ -102,57 +62,3 @@ class Trajectory(LightTrajectory):
 
         # Precompute the identity transition matrix.
         self.P_nx_identity = get_identity_P_nx(set(Q_nx))
-
-
-
-class Event(object):
-    def __init__(self, track=None, tm=None, sa=None, sb=None):
-        """
-
-        Parameters
-        ----------
-        track : Trajectory object, optional
-            the trajectory object on which the event occurs
-        tm : float, optional
-            time along the edge at which the event occurs
-        sa : hashable, optional
-            initial state of the transition
-        sb : hashable, optional
-            final state of the transition
-
-        """
-        self.track = track
-        self.tm = tm
-        self.sa = sa
-        self.sb = sb
-
-    def init_sa(self, state):
-        if self.sa is not None:
-            raise Exception('the initial state is already set')
-        self.sa = state
-
-    def init_sb(self, state):
-        if self.sb is not None:
-            raise Exception('the final state is already set')
-        self.sb = state
-
-    def init_or_confirm_sb(self, state):
-        if self.sb is None:
-            self.sb = state
-        if self.sb != state:
-            raise Exception('final state incompatibility')
-
-    def __repr__(self):
-        return 'Event(track=%s, tm=%s, sa=%s, sb=%s)' % (
-                self.track, self.tm, self.sa, self.sb)
-
-    def __lt__(self, other):
-        """
-        Give events a partial order.
-
-        """
-        if self.tm == other.tm:
-            warnings.warn('simultaneous events')
-        return self.tm < other.tm
-
-
