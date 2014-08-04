78f0c57486a949e627597b6480aece4070012604
alex <argriffi@ncsu.edu>
2014-08-04 18:52:16 -0400

    delegate part of the tree partitioning to nxctmctree

--- nxblink/navigation.py
+++ nxblink/navigation.py
@@ -8,41 +8,7 @@ from __future__ import division, print_function, absolute_import
 
 import networkx as nx
 
-
-def partition_nodes(T, edge_to_blen, edge_to_rate):
-    """
-    Partition nodes of a tree.
-
-    The nodes are partitioned into equivalence classes,
-    where two nodes are considered equivalent if they are connected
-    by a branch of length exactly zero.
-
-    Parameters
-    ----------
-    T : networkx DiGraph
-        The tree.
-    edge_to_blen : dict
-        Maps directed edges of T to non-negative branch lengths.
-    edge_to_rate : dict
-        Maps directed edges of T to non-negative rate scaling factors.
-
-    Returns
-    -------
-    components : list of list of nodes
-        Each list contains the nodes of a connected component.
-
-    """
-    # Compute an undirected graph representing an equivalence relation.
-    G = nx.Graph()
-    G.add_nodes_from(T)
-    for edge in T.edges():
-        blen = edge_to_blen[edge]
-        rate = edge_to_rate[edge]
-        if (not blen) or (not rate):
-            G.add_edge(*edge)
-
-    # Return the connected components of the graph.
-    return nx.connected_components(G)
+from nxctmctree.navigation import partition_nodes
 
 
 def gen_segments(edge, node_to_tm, tracks):
